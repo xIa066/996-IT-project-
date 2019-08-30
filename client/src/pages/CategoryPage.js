e12ff0bcea48eb7a07891b2e5628d21534daec1f
Akel Saleh <akel.saleh15@gmail.com>
2019-08-30 11:08:30 +1000

    Navbar component

---
+++ client/src/pages/CategoryPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CategoryPage extends Component {
+  render(){
+    return(
+      <div>
+        CategoryPage
+      </div>
+    );
+  }
+}
+
+export default CategoryPage;
---
+++ client/src/pages/SocialPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SocialPage extends Component {
+  render(){
+    return(
+      <div>
+        SocialPage
+      </div>
+    );
+  }
+}
+
+export default SocialPage;
