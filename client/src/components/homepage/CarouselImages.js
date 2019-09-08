6317ef179e22b2d63ac7cb5a7bbf8c8fa72e2841
Akel Saleh <akel.saleh15@gmail.com>
2019-09-09 03:00:59 +1000

    Carousel on homepage added (using a instead of link in bootstrap for it...)

---
+++ client/src/components/homepage/CarouselImages.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './carousel.css';
+import famImage from '../../images/slide2.jpg';
+import secondImage from '../../images/recent2.jpg';
+import prevButton from '../../images/left.png';
+import nextButton from '../../images/right.png';
+
+class CarouselImages extends React.Component {
+  render(){
+    return(
+      <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={famImage} className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src={secondImage} className="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <img src={prevButton} alt="Previous" />
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <img src={nextButton} alt="Previous" />
+        </a>
+      </div>
+    );
+  }
+}
+
+export default CarouselImages;
--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -6,20 +6,18 @@ import './navbar.css'
 class Navbar extends React.Component {
   render(){
     return(
-      <div id="top" className="navbar navbar-expand-lg navbar-dark fixed-top py-0" role="navigation">
+      <div id="top" className="navbar navbar-expand-lg navbar-dark fixed-top py-1" role="navigation">
           <div className="container">
-              <div className="navbar-header" >
-                  <Link className="navbar-brand" to="/"><strong>Bloodlines,</strong>Tales and Roots</Link>
-              </div>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul className="nav navbar-nav ml-auto">
-                  <li className="active"><Link className="nav-link" to="/">Home</Link></li>
-                  <li><Link className="nav-link" to="/about">About</Link></li>
-                  <li><Link className="nav-link" to="/category">Category</Link></li>
-                  <li><Link className="nav-link" to="/gallery">Gallery</Link></li>
-                  <li><Link className="nav-link" to="/social">Social</Link></li>
-                </ul>
-              </div>
+          <Link className="navbar-brand" to="/"><strong>Bloodlines,</strong>Tales and Roots</Link>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <ul className="nav navbar-nav ">
+              <li className="active"><Link className="nav-link" to="/">Home</Link></li>
+              <li><Link className="nav-link" to="/about">About</Link></li>
+              <li><Link className="nav-link" to="/category">Category</Link></li>
+              <li><Link className="nav-link" to="/gallery">Gallery</Link></li>
+              <li><Link className="nav-link" to="/social">Social</Link></li>
+            </ul>
+          </div>
           </div>
       </div>
     );
