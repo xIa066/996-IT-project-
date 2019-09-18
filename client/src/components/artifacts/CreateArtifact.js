ce5e3e4a38f8d637d6801cfe6c3db9e8fde1f0b7
Akel Saleh <akel.saleh15@gmail.com>
2019-09-18 14:36:55 +1000

    added upload files functionality - needs to link to s3 and CloudFront

--- client/src/components/artifacts/CreateArtifact.js
+++ client/src/components/artifacts/CreateArtifact.js
@@ -3,6 +3,8 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createArtifact } from '../../actions';
 
+import './imageInput.css';
+
 class CreateArtifact extends React.Component {
   renderError({ error, touched }){
     return(
@@ -29,8 +31,39 @@ class CreateArtifact extends React.Component {
     );
   }
 
+  handleImageInput = functionType => e => {
+    functionType(e.target.files[0]);
+  }
+
+
+ renderImageInput = ({
+    input: {
+      value: omitValue,
+      onChange,
+      onBlur,
+      ...inputProps,
+    },
+    meta: omitMeta,
+    ...props,
+  }) => {
+    return(
+    <div>
+    <input
+      onChange={this.handleImageInput(onChange)}
+      type="file"
+      {...inputProps}
+      {...props}
+      className="image-input"
+      id="imageInput"
+    />
+    {/* <label htmlFor="imageInput">Choose a Picture</label> */}
+    </div>
+    );
+  }
+
   onSubmit = (formValues) => {
     this.props.createArtifact(formValues);
+    
   }
 
   render(){
@@ -38,9 +71,9 @@ class CreateArtifact extends React.Component {
       <form className="needs-validation" onSubmit={this.props.handleSubmit(this.onSubmit)} noValidate>
         <Field name="name" component={this.renderInput} label="Enter Name" placeholder="Title..." id="inputTitle" />
         <Field name="date" component={this.renderInput} label="Enter Date" placeholder="DD/MM/YYYY" id="inputDate" />
-        <Field name="photo" component={this.renderInput} label="Enter Photo URL" placeholder="Photo URL" id="inputPhoto" />
         <Field name="ownerID" component={this.renderInput} label="Enter User ID" placeholder="00000" id="inputID" />
         <Field name="description" component={this.renderInput} label="Enter Description" placeholder="Description..." id="inputDesc" />
+        <Field name="photo" component={this.renderImageInput} label="Enter Photo URL" placeholder="Photo URL" id="inputPhoto" /><br/>
         <input className="btn btn-secondary" type="submit" value="Submit" />
       </form>
     );
