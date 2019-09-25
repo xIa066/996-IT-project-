6a70cc7a7661af728d254765fb3fa98b6bbceb62
Guowei Chen <g.chen23@student.unimelb.edu.au>
2019-09-26 09:45:44 +1000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,25 @@ const Users = mongoose.model('user');
 
 
 
-module.exports.findAllArtifact = findAllArtifact;
-module.exports.createArtifact = createArtifact;
-module.exports.findArtifactByOwner = findArtifactByOwner;
+// creat a new user
+var createUser = function(req, res){
+    var user = new Users({
+        "name": req.body.name,
+        "userID": req.body.userID,
+        "email": req.body.email,
+        "birth": req.body.birth,
+        "bio": req.body.bio,
+        "photo": req.body.photo
+    });
+    user.save(function(err, user){
+        if(!err){
+            res.send(user);
+        }
+        else{
+            res.sendStatus(400);
+        }
+    });
+};
+
+
+module.exports.createUser = createUser;
